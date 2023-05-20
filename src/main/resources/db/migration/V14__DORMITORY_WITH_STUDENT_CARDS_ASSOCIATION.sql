4e93aefa752d9c7e202c49cb1cf5c1dc82fbb2b3
odeyalo <theprogrammerinfuture@gmail.com>
2023-05-20 15:57:30 +0300

    Written sql migrations

---
+++ src/main/resources/db/migration/V14__DORMITORY_WITH_STUDENT_CARDS_ASSOCIATION.sql
@@ -0,0 +1,7 @@
+CREATE TABLE registered_student_cards(
+    dormitory_id BIGINT NOT NULL,
+    student_cards_id BIGINT NOT NULL UNIQUE,
+
+    CONSTRAINT dormitory_id_fk FOREIGN KEY (dormitory_id) REFERENCES dormitory (id),
+    CONSTRAINT student_cards_id_fk FOREIGN KEY (student_cards_id) REFERENCES student(id)
+)
